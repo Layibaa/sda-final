50b4e092362d7fc871d2e37473b6add0a5ecacb1
Layiba <layibakafayat@gmail.com>
2024-12-12 20:46:38 +0500

    ..

--- src/BackendCode/Car.java
+++ src/BackendCode/Car.java
@@ -1,30 +1,32 @@
 package BackendCode;
 
-import java.io.*;
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.ArrayList;
 
 /**
- * The Car class holds car attributes and implements business logic related to car data.
- * Refactored to adhere to SOLID principles.
+ *
+ * @author @AbdullahShahid01
  */
 public class Car implements Serializable {
 
-    // Car Attributes
     private int ID;
     private String Maker, Name, Colour, Type;
-    private int SeatingCapacity;
-    private String Model, Condition, RegNo;
+    int SeatingCapacity;
+    String Model, Condition, RegNo;
     private int RentPerHour;
     private CarOwner carOwner;
 
-    // Constants to replace magic numbers/strings
-    private static final String CAR_FILE_PATH = "Car.ser";
-    private static final String REGEX_REG_NO = "^[A-Za-z]+-\\d+$";  // RegNo format: letters-digits
+    public Car() {
+    }
 
-    // Default Constructor
-    public Car() {}
-
-    // Parametrized Constructor
     public Car(int ID, String Maker, String Name, String Colour, String Type, int SeatingCapacity, String Model, String Condition, String RegNo, int RentPerHour, CarOwner carOwner) {
         this.ID = ID;
         this.Maker = Maker;
@@ -39,7 +41,6 @@ public class Car implements Serializable {
         this.carOwner = carOwner;
     }
 
-    // Getters and Setters
     public int getID() {
         return ID;
     }
@@ -130,85 +131,222 @@ public class Car implements Serializable {
 
     @Override
     public String toString() {
-        return "Car{" + "ID=" + ID + ", Maker=" + Maker + ", Name=" + Name + ", Colour=" + Colour + ", Type=" + Type + ", SeatingCapacity=" + SeatingCapacity + ", Model=" + Model + ", Condition=" + Condition + ", RegNo=" + RegNo + ", RentPerHour=" + RentPerHour + ", carOwner=" + carOwner + '}';
+        return "Car_new{" + "ID=" + ID + ", Maker=" + Maker + ", Name=" + Name + ", Colour=" + Colour + ", \nType=" + Type + ", SeatingCapacity=" + SeatingCapacity + ", Model=" + Model + ", Condition=" + Condition + ", RegNo=" + RegNo + ", RentPerHour=" + RentPerHour + ", \ncarOwner=" + carOwner.toString() + '}' + "\n";
     }
 
-    // Refactored Add Method
     public void Add() {
-        ArrayList<Car> cars = CarPersistence.readCars();
-        this.ID = (cars.isEmpty()) ? 1 : cars.get(cars.size() - 1).ID + 1;  // Auto ID Generation
-        cars.add(this);
-        CarPersistence.writeCars(cars);  // Externalize file writing
+        ArrayList<Car> car = Car.View();
+        if (car.isEmpty()) {
+            this.ID = 1;
+        } else {
+            this.ID = car.get(car.size() - 1).ID + 1; // Auto ID...
+        }
+        car.add(this);
+        File file = new File("Car.ser");
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException ex) {
+                System.out.println(ex);
+            }
+        }
+        ObjectOutputStream outputStream = null;
+        try {
+            outputStream = new ObjectOutputStream(new FileOutputStream(file));
+            for (int i = 0; i < car.size(); i++) {
+                outputStream.writeObject(car.get(i));
+            }
+        } catch (FileNotFoundException ex) {
+            System.out.println(ex);
+        } catch (IOException ex) {
+            System.out.println(ex);
+        } finally {
+            if (outputStream != null) {
+                try {
+                    outputStream.close();
+                } catch (IOException ex) {
+                    System.out.println(ex);
+                }
+            }
+        }
     }
 
-    // Refactored Update Method
+    /**
+     * aik new Object bna k us se Update() ka method call krte hein aur us new
+     * object mein jo Car ID hai agar usi ID ki koi car pehle mojood ha tou us
+     * se replace ho jay gi
+     */
     public void Update() {
-        ArrayList<Car> cars = CarPersistence.readCars();
-        for (int i = 0; i < cars.size(); i++) {
-            if (cars.get(i).ID == ID) {
-                cars.set(i, this);  // Replace car with matching ID
-                break;
+        ArrayList<Car> car = Car.View();
+
+        // for loop for replacing the new Car object with old one with same ID
+        for (int i = 0; i < car.size(); i++) {
+            if (car.get(i).ID == ID) {
+                car.set(i, this);
             }
         }
-        CarPersistence.writeCars(cars);  // Externalize file writing
+
+        // code for writing new Car record 
+        ObjectOutputStream outputStream = null;
+        try {
+            outputStream = new ObjectOutputStream(new FileOutputStream("Car.ser"));
+            for (int i = 0; i < car.size(); i++) {
+                outputStream.writeObject(car.get(i));
+            }
+        } catch (FileNotFoundException ex) {
+            System.out.println(ex);
+        } catch (IOException ex) {
+            System.out.println(ex);
+        } finally {
+            if (outputStream != null) {
+                try {
+                    outputStream.close();
+                } catch (IOException ex) {
+                    System.out.println(ex);
+                }
+            }
+        }
     }
 
-    // Refactored Remove Method
     public void Remove() {
-        ArrayList<Car> cars = CarPersistence.readCars();
-        cars.removeIf(car -> car.ID == this.ID);  // Simplified removal using removeIf
-        CarPersistence.writeCars(cars);  // Externalize file writing
-    }
 
-    // Search Methods
-    public static ArrayList<Car> SearchByName(String name) {
-        ArrayList<Car> cars = CarPersistence.readCars();
-        ArrayList<Car> result = new ArrayList<>();
-        for (Car car : cars) {
-            if (car.Name.equalsIgnoreCase(name)) {
-                result.add(car);
+        ArrayList<Car> car = Car.View();
+        // for loop for deleting the required Car
+        for (int i = 0; i < car.size(); i++) {
+            if ((car.get(i).ID == ID)) {
+                car.remove(i);
             }
         }
-        return result;
+        // code for writing new Car record 
+        ObjectOutputStream outputStream = null;
+        try {
+            outputStream = new ObjectOutputStream(new FileOutputStream("Car.ser"));
+            for (int i = 0; i < car.size(); i++) {
+                outputStream.writeObject(car.get(i));
+            }
+        } catch (FileNotFoundException ex) {
+            System.out.println(ex);
+        } catch (IOException ex) {
+            System.out.println(ex);
+        } finally {
+            if (outputStream != null) {
+                try {
+                    outputStream.close();
+                } catch (IOException ex) {
+                    System.out.println(ex);
+                }
+            }
+        }
+    }
+
+    public static ArrayList<Car> SearchByName(String name) {
+        ArrayList<Car> car = Car.View();
+        ArrayList<Car> s = new ArrayList<>();
+        for (int i = 0; i < car.size(); i++) {
+            if (car.get(i).Name.equalsIgnoreCase(name)) {
+                s.add(car.get(i));
+            }
+        }
+        return s;
     }
 
     public static Car SearchByID(int id) {
-        ArrayList<Car> cars = CarPersistence.readCars();
-        for (Car car : cars) {
-            if (car.ID == id) {
-                return car;
+        ArrayList<Car> car = Car.View();
+        for (int i = 0; i < car.size(); i++) {
+            if (car.get(i).ID == id) {
+                return car.get(i);
             }
         }
         return null;
     }
 
     public static Car SearchByRegNo(String regNo) {
-        ArrayList<Car> cars = CarPersistence.readCars();
-        for (Car car : cars) {
-            if (car.RegNo.equalsIgnoreCase(regNo)) {
-                return car;
+        ArrayList<Car> car = Car.View();
+        for (int i = 0; i < car.size(); i++) {
+            if (car.get(i).RegNo.equalsIgnoreCase(regNo)) {
+                return car.get(i);
             }
         }
         return null;
     }
 
-    // Validation Methods
-    public static boolean isNameValid(String name) {
-        return name != null && name.matches("[A-Za-z0-9 ]+");  // Improved regex for valid names
+    public static ArrayList<Car> View() {
+        ArrayList<Car> carList = new ArrayList<>(0);
+        ObjectInputStream inputStream = null;
+        try {
+// open file for reading
+            inputStream = new ObjectInputStream(new FileInputStream("Car.ser"));
+            boolean EOF = false;
+// Keep reading file until file ends
+            while (!EOF) {
+                try {
+                    Car myObj = (Car) inputStream.readObject();
+                    carList.add(myObj);
+                } catch (ClassNotFoundException e) {
+                    System.out.println(e);
+                } catch (EOFException end) {
+                    EOF = true;
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        } catch (IOException e) {
+            System.out.println(e);
+        } finally {
+            try {
+                if (inputStream != null) {
+                    inputStream.close();
+                }
+            } catch (IOException e) {
+                System.out.println(e);
+            }
+        }
+        return carList;
     }
 
-    public static boolean isRegNoValid(String regNo) {
-        return regNo != null && regNo.matches(REGEX_REG_NO);  // Improved RegNo validation
+    public static boolean isNameValid(String Name) {
+        boolean flag = false;
+        for (int i = 0; i < Name.length(); i++) {
+//            Name can contain white spaces
+            if (Character.isLetter(Name.charAt(i)) |Character.isDigit(Name.charAt(i))| Name.charAt(i) == ' ') {
+                flag = true;
+            } else {
+                flag = false;
+                break;
+            }
+        }
+        return flag;
     }
 
-    // Rented Check
+    public static boolean isRegNoValid(String RegNo) {
+        // reg no must contain letters followed by digits, both separated by '-' dash
+        // EXAMPLE: ASD-2343
+        String[] token = RegNo.split("-");
+        if (token.length == 2) {
+            for (int i = 0; i < token[0].length(); i++) {
+                if (!Character.isLetter(token[0].charAt(i))) {
+                    return false;
+                }
+            }
+            for (int i = 0; i < token[1].length(); i++) {
+                if (!Character.isDigit(token[1].charAt(i))) {
+                    return false;
+                }
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     public boolean isRented() {
-        ArrayList<Car> bookedCars = Booking.getBookedCars();
-        for (Car car : bookedCars) {
-            if (car.ID == this.ID) {
+        ArrayList<Car> BookedCars = Booking.getBookedCars();
+        for (int i = 0; i < BookedCars.size(); i++) {
+            if (BookedCars.get(i).ID == this.ID) {
                 return true;
             }
         }
         return false;
     }
+
 }
